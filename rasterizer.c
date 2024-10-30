a4e4858ada26630ea62a947cbe2ffa83f3261f4c
Lukas <lukas.weber26@gmail.com>
2024-10-30 17:37:14 +0000

    triangle interiors completed

--- rasterizer.c
+++ rasterizer.c
@@ -201,6 +201,10 @@ int int_array_get_index(int_array array, int index) {
 	return array.arena->storage[array.offset + index];
 }
 
+void int_array_drop_last(int_array * array) {
+	array->n_integers -= 1;
+}
+
 int_array interpolate(scene s, int i0, int d0, int i1, int d1) {
 	int_array new_array = int_array_create(s.scene_arena);
 	if (i0 == i1) {
@@ -214,6 +218,20 @@ int_array interpolate(scene s, int i0, int d0, int i1, int d1) {
 		d = d+a;
 	}
 	return new_array;
+}
+
+int_array arena_drop_element(scene s, int_array a){
+	int_array output = a;
+	output.n_integers -= 1;
+	s.scene_arena->n_items -= 1;
+	return output;
+}
+
+void int_array_print(int_array input) {
+	for (int i = input.offset; i < input.offset + input.n_integers; i ++) {
+		printf("%d,", input.arena->storage[i]);
+	}
+	printf("\n");
 }
 
 void drawline(scene s, canvas_point p0, canvas_point p1, rgb_color color) {
@@ -253,15 +271,59 @@ typedef struct triangle {
 	canvas_point p2;	
 	canvas_point p3;	
 	rgb_color color;
+	rgb_color outline_color;
 } triangle; 
 
-void draw_triangle(scene s, triangle t) {
-	drawline(s, t.p1, t.p2, t.color);
-	drawline(s, t.p1, t.p3, t.color);
-	drawline(s, t.p2, t.p3, t.color);
+int_array int_array_cat(int_array a, int_array b) {
+	assert(a.arena == b.arena);
+	assert(a.offset + a.n_integers == b.offset);
+	int_array result;
+	result.offset = a.offset; 
+	result.arena = a.arena;
+	result.n_integers =  a.n_integers + b.n_integers;
+	return result;
 }
 
-triangle triangle_create (int x1, int y1, int x2, int y2, int x3, int y3, unsigned char r, unsigned char g, unsigned char b) {
+void draw_triangle_outline(scene s, triangle t) {
+	//outline
+	drawline(s, t.p1, t.p2, t.outline_color);
+	drawline(s, t.p1, t.p3, t.outline_color);
+	drawline(s, t.p2, t.p3, t.outline_color);
+}
+
+void draw_triangle_interior(scene s, triangle t) {
+	assert(t.p1.y < t.p2.y);
+	assert(t.p2.y < t.p3.y);
+
+	//create two lists of x coordinates... (via interpolation and a bit of arena magic to avoid copying arrays)
+	int_array x01 = interpolate(s, t.p1.y, t.p1.x, t.p2.y, t.p2.x);	
+	int_array x12 = interpolate(s, t.p2.y, t.p2.x, t.p3.y, t.p3.x);	
+	int_array x012 = int_array_cat(x01, x12);
+	int_array x02 = interpolate(s, t.p1.y, t.p1.x, t.p3.y, t.p3.x);	
+
+	//determine which item is left 
+	int_array x_left = x012;
+	int_array x_right = x02;
+
+	int m = (int)floor(((double)x012.n_integers)/2.0);
+	if (int_array_get_index(x02,m) < int_array_get_index(x012,m)) {
+		x_left = x02;
+		x_right = x012;
+	}
+
+	for (int y = t.p1.y; y < t.p3.y; y++) {
+		for (int x = int_array_get_index(x_left, y-t.p1.y); x < int_array_get_index(x_right, y-t.p1.y); x++) {
+			put_pixels_on_canvas(s, create_point(x,y), t.color);
+		}
+	}	
+}
+
+void draw_triangle(scene s, triangle t) {
+	draw_triangle_outline(s,t);
+	//draw_triangle_interior(s, t);
+}
+
+triangle triangle_create (int x1, int y1, int x2, int y2, int x3, int y3, unsigned char r, unsigned char g, unsigned char b, unsigned char r2, unsigned char g2, unsigned char b2) {
 	canvas_point p1 = create_point(x1,y1);
 	canvas_point p2 = create_point(x2,y2);
 	canvas_point p3 = create_point(x3,y3);
@@ -285,7 +347,7 @@ triangle triangle_create (int x1, int y1, int x2, int y2, int x3, int y3, unsign
 		p2 = temp;
 	}
 
-	triangle new_triangle = {p1,p2,p3,create_color(r,g,b)};
+	triangle new_triangle = {p1,p2,p3,create_color(r,g,b), create_color(r2,g2,b2)};
 	return new_triangle;
 }
 
@@ -302,9 +364,8 @@ int main() {
 	new_scene.screen = calloc(3*new_scene.screen_height*new_scene.screen_width, sizeof(unsigned char));
 	new_scene.scene_arena = int_arena_create(10000);
 
-	//drawline(new_scene, create_point(400,200), create_point(400,400), create_color(240,32,15));
-	triangle new_triangle = triangle_create(200, 300, -200, -200, 500, 400, 42, 212, 40);
-	draw_triangle(new_scene, new_triangle);
+	triangle new_triangle = triangle_create(200, 300, -200, -200, 200, 200, 20, 210, 200, 254, 20, 20);
+	draw_triangle(new_scene, new_triangle); 
 
 	GLFWwindow  * window = opengl_init(&VAO, &program, &texture);	
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB ,GL_UNSIGNED_BYTE, new_scene.screen);
@@ -326,11 +387,12 @@ int main() {
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	
+			
+		new_scene.scene_arena->n_items = 0;
 		draw_triangle(new_scene, new_triangle);
 
 		i++;
-		if (i > 100) {
+		if (i > 1000) {
 			exit(0);
 		}
 
